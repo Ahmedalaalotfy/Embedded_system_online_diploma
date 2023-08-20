ddb9b4fbf50c73763186aedd8daa9a8b1f4d730a
ahmedlotfy <ahmed@yahoo.com>
2023-08-21 01:27:40 +0300

    unit7_lesson4_EXTI_Driver

---
+++ unit7_MCU_Essential_Peripherals/unit7_lesson3_EXTI_Driver/Drivers/Stm32_F103C6_Drivers/HAL/KPD_Driver/KPD_interface.h
@@ -0,0 +1,19 @@
+/*
+ * KPD_interface.h
+ *
+ * Created: 9/13/2022 10:03:04 AM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef KPD_INTERFACE_H_
+#define KPD_INTERFACE_H_
+
+#define KPD_NOT_PRESSED     0xff
+
+void KPD_GPIO_Init(void);
+void KPD_getValue(uint8_t *returnedValue);
+
+
+
+#endif /* KPD_INTERFACE_H_ */
