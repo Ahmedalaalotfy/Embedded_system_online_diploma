bd560d5b990063eaea5362409ab455504410aa69
Ahmedalaalotfy <63472034+Ahmedalaalotfy@users.noreply.github.com>
2023-09-01 07:49:15 +0300

    Add files via upload

---
+++ unit8_MCU_Interfacing/unit8_lesson5_SPI_STM32/Lab1/Drivers/Debug/sources.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+EXECUTABLES := 
+OBJS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Src \
+Startup \
+Stm32_F103C6_Drivers/HAL/KPD_Driver \
+Stm32_F103C6_Drivers/HAL/LCD_Driver \
+Stm32_F103C6_Drivers/MCAL/EXTI \
+Stm32_F103C6_Drivers/MCAL/GPIO \
+Stm32_F103C6_Drivers/MCAL/RCC \
+Stm32_F103C6_Drivers/MCAL/SPI \
+Stm32_F103C6_Drivers/MCAL/USART \
+
