18c81206ef1d526175d9cbc3fa015b8a4fcbcfd7
Ahmedalaalotfy <63472034+Ahmedalaalotfy@users.noreply.github.com>
2023-09-01 07:37:56 +0300

    Add files via upload

---
+++ unit8_MCU_Interfacing/unit8_lesson2_UART_Atmega32/unit8_lesson2_Section/unit8_lesson2_Section/STD_TYPES.h
@@ -0,0 +1,26 @@
+/*
+name : Ahmedalaalotfy
+date : 1/9/2002
+version : 1.0
+swc : Standerd types 
+describtion :
+
+*/
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int     u16;
+typedef signed short int       s16;
+
+typedef unsigned long int      u32;
+typedef signed long int        s32;
+
+typedef float                  f32;
+typedef double                 f64;
+
+#define  NULL 0 
+
+#endif 
